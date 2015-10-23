3ac63dcec05d87bcc843c9376f526d23f4e2fcf1
Dmytro Pavlenko <atosdo@live.com>
2015-10-23 21:34:05 +0300

    Ex 1.3 other variant was added.

--- Chapter 1/Ex 1.3.rkt
+++ Chapter 1/Ex 1.3.rkt
@@ -21,4 +21,21 @@
 (= (sum-of-squares-max2 1 5 5) 50)
 (= (sum-of-squares-max2 6 8 10) 164)
 (= (sum-of-squares-max2 10 5 11) 221)
+;-------------------------------------------------------------------
+; Еще вариант реализации: находим сумму квадратов всех 3х чисел и отнимаем квадрат минимального из них.
+(define (min2 a b)
+  (if (< a b)
+      a
+      b))
+
+(define (min3 a b c)
+  (min2 a (min2 b c)))
+
+(define (other-sum-of-squares-max2 a b c)
+  (- (+ (square a) (square b) (square c))
+     (square (min3 a b c))))
+
+(= (other-sum-of-squares-max2 1 5 5) 50)
+(= (other-sum-of-squares-max2 6 8 10) 164)
+(= (other-sum-of-squares-max2 10 5 11) 221)
 ;===================================================================
